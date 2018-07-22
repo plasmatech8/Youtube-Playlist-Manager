717fbcaeb73676c31998395b5f2f0bf83f4155eb
plasmatech8 <com0004@gmail.com>
2018-07-22 20:46:01 +1000

    initialised with extension template

---
+++ popup.js
@@ -0,0 +1,30 @@
+
+function init() {
+		
+	// var total = document.getElementById('total');
+	// var limit = document.getElementById('limit');
+	// var amount = document.getElementById('amount');
+	// var spendBtn = document.getElementById('spendAmount');
+
+	// Initialisation
+	// chrome.storage.sync.get(['total', 'limit'], function(budget){
+	// 	// Can use get 'total' or ['total', 'limit']
+	// 	if (budget.total) 
+	// 		total.innerText = budget.total;
+	// 	if (budget.limit)
+	// 		limit.innerText = budget.limit;
+	// });
+	
+	// Events	
+	// spendBtn.addEventListener('click', function () {
+
+	// 	// Get the object stored under the name 'total'
+	// 	chrome.storage.sync.get(['total','limit'], function(budget){
+			
+			
+			
+	// 	});
+	// });
+}
+
+window.onload = init;
